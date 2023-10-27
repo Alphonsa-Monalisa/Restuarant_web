138aab25463f4cd5f041a9c529423ded76bd2e77
Alphonsa-Monalisa <alphonsamonalisa19@gmail.com>
2023-10-27 15:27:48 +0530

    setup

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -28,6 +28,7 @@ const Footer = () => (
           src={images.spoon}
           className="spoon__img"
           style={{ marginTop: 15 }}
+          alt="imgspoon"
         />
         <div className="app__footer-links_icons">
           <FiFacebook />
